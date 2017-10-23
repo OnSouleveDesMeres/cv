caa6431d988f0899d20cce5fbfc0d05a1364bd39
GodFuckABitch <sylvain.combraque@etudiant.univ-reims.fr>
2017-10-23 13:16:03 +0200

    fix mail

--- sendMail.php
+++ sendMail.php
@@ -9,25 +9,25 @@
 
 require_once 'PHPMailer/PHPMailerAutoload.php';
 
-emailActivation( $_GET['mail'], $_GET['subject'], $_GET['content']);
+$mailFrom = $_GET['mail'];
+$subject = $_GET['subject'];
+$content = $_GET['content'];
 
-function emailActivation($mailFrom, $subject, $content){
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'compte.asfeld@gmail.com';
-    $mail->Password = '6edb53506f062ec8b774f30fcd6dea3fd376a713';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-    $mail->setFrom('compte.asfeld@gmail.com', 'Compte');
-    $mail->addAddress('sylvaincombraque@hotmail.fr');
-    $mail->isHTML(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->Subject = "{$subject}";
-    $mail->Body    = "Email de {$mailFrom}<br>
-                        <br>
-                        {$content}";
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    $mail->send();
-}
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'compte.asfeld@gmail.com';
+$mail->Password = '6edb53506f062ec8b774f30fcd6dea3fd376a713';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+$mail->setFrom('compte.asfeld@gmail.com', 'Compte');
+$mail->addAddress('sylvaincombraque@hotmail.fr');
+$mail->isHTML(true);
+$mail->CharSet = 'UTF-8';
+$mail->Subject = "{$subject}";
+$mail->Body    = "Email de {$mailFrom}<br>
+                    <br>
+                    {$content}";
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->send();
